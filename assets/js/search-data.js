6693647cac3f6af92e2a76dd85933ff8885c5166
abhi25072002 <72749889+abhi25072002@users.noreply.github.com>
2025-09-12 06:27:31 +0000

    Deploying to gh-pages from @ abhi25072002/abhi25072002.github.io@952db4758e57fc8594c48ed9d94ac626a8eabd14 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,108 +30,86 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "projects-project-1",
+        },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
+              window.location.href = "/projects/1_project.html";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
+              window.location.href = "/projects/2_project.html";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
+              window.location.href = "/projects/3_project.html";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
+              window.location.href = "/projects/4_project.html";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
+              window.location.href = "/projects/5_project.html";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
+              window.location.href = "/projects/6_project.html";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
+              window.location.href = "/projects/7_project.html";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
+              window.location.href = "/projects/8_project.html";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/9_project.html";
             },},{id: "projects-arbitrary-precision-calculator-c",
           title: 'Arbitrary-Precision Calculator (C)',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/arbitrary-precision-calculator/";
+              window.location.href = "/projects/arbitrary-precision-calculator.html";
             },},{id: "projects-banktrack-banking-application",
           title: 'BankTrack - Banking Application',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/banktrack/";
+              window.location.href = "/projects/banktrack.html";
             },},{id: "projects-http-server-computer-networks",
           title: 'HTTP Server - Computer Networks',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/http-server/";
+              window.location.href = "/projects/http-server.html";
             },},{id: "projects-paint-application-object-oriented-programming-c",
           title: 'Paint Application - Object Oriented Programming (C++)',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/paint-application/";
+              window.location.href = "/projects/paint-application.html";
             },},{id: "projects-sustainability-benchmark-automation",
           title: 'Sustainability Benchmark Automation',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/sustainability-benchmark-automation/";
+              window.location.href = "/projects/sustainability-benchmark-automation.html";
             },},{id: "projects-user-threads-operating-systems",
           title: 'User-Threads - Operating Systems',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/user-threads-os/";
+              window.location.href = "/projects/user-threads-os.html";
             },},{id: "projects-weed-detection-using-vision-transformers-vit",
           title: 'Weed Detection using Vision Transformers (ViT)',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/weed-detection-vit/";
+              window.location.href = "/projects/weed-detection-vit.html";
             },},{
         id: 'social-email',
         title: 'email',
